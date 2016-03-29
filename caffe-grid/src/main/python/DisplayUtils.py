0395ccbe0a01fb0d9fef8d1a2f69a86d1383aadb
Mridul Jain <mridul@yahoo-inc.com>
2016-03-29 15:06:42 -0700

    Generate network graph

--- caffe-grid/src/main/python/DisplayUtils.py
+++ caffe-grid/src/main/python/DisplayUtils.py
@@ -3,7 +3,11 @@ from io import BytesIO
 from IPython.display import HTML
 import numpy as np
 from base64 import b64encode
-from draw_net import parse_args
+from google.protobuf import text_format
+
+import caffe
+import caffe.draw
+from caffe.proto import caffe_pb2
 
 def get_np_array(row):
     return np.frombuffer(row.data, 'uint8').reshape((row.height,row.width))
@@ -27,9 +31,12 @@ def show_df(df, nrows=10):
     html += "</table>"
     return HTML(html)
 
-def show_network(input_net_proto_file, output_image_file):
-    args = parse_args()
+def show_network(input_net_proto_file, rankdir):
     net = caffe_pb2.NetParameter()
-    text_format.Merge(open(args.input_net_proto_file).read(), net)
-    print('Drawing net to %s' % args.output_image_file)
-    caffe.draw.draw_net_to_file(net, args.output_image_file, args.rankdir)
+    text_format.Merge(open(input_net_proto_file).read(), net)
+    image=caffe.draw.draw_net(net, rankdir)
+    html = "<body>"
+    html += "<img src='data:image/png;base64," + b64encode(image) + "' />"
+    html += "</body>"
+    return HTML(html)
+    
