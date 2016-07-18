01927080de6c18580808700726658b56139b2d52
Mridul Jain <mridul@yahoo-inc.com>
2016-07-18 15:23:35 -0700

    changes to travis

--- scripts/travis/travis_install.sh
+++ scripts/travis/travis_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # This script must be run with sudo.
 # for libopencv-dev-2.4
-add-apt-repository -y ppa:kubuntu-ppa/backports
+add-apt-repository -y --force-yes ppa:kubuntu-ppa/backports
 # for proto2.5
-add-apt-repository -y ppa:chris-lea/protobuf
-apt-get -y update
+add-apt-repository -y --force-yes ppa:chris-lea/protobuf
+apt-get -y --force-yes update
 # remove CONDA directory
 rm -rf $CONDA_DIR
 # invoke BVLC caffe scripts
