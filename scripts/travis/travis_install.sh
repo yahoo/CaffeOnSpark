a428c7d5be064b7f7fa018fea87cf66c2705108c
Mridul Jain <mridul@yahoo-inc.com>
2016-08-22 20:06:47 -0700

    Libboost dependencies

--- scripts/travis/travis_install.sh
+++ scripts/travis/travis_install.sh
@@ -4,11 +4,15 @@
 add-apt-repository -y ppa:kubuntu-ppa/backports
 # for proto2.5
 add-apt-repository -y  ppa:chris-lea/protobuf
-# for boost
-add apt-repository -y ppa:boost-latest/ppa
 apt-get -y --force-yes update
 # remove CONDA directory
 rm -rf $CONDA_DIR
 # invoke BVLC caffe scripts
 ./caffe-public/scripts/travis/travis_install.sh
-./scripts/travis/travis_test_install.sh
+export CAFFE_ON_SPARK=$(pwd)
+${CAFFE_ON_SPARK}/scripts/local-setup-hadoop.sh 2>&1 > /dev/null
+export HADOOP_HOME=$(pwd)/hadoop-2.6.4
+export PATH=${HADOOP_HOME}/bin:${PATH}
+${CAFFE_ON_SPARK}/scripts/local-setup-spark.sh 2>&1 > /dev/null
+export SPARK_HOME=$(pwd)/spark-1.6.0-bin-hadoop2.6
+export PATH=${SPARK_HOME}/bin:${PATH}
--- scripts/travis/travis_test_install.sh
+++
@@ -1,7 +0,0 @@
-export CAFFE_ON_SPARK=$(pwd)
-${CAFFE_ON_SPARK}/scripts/local-setup-hadoop.sh 2>&1 > /dev/null
-export HADOOP_HOME=$(pwd)/hadoop-2.6.4
-export PATH=${HADOOP_HOME}/bin:${PATH}
-${CAFFE_ON_SPARK}/scripts/local-setup-spark.sh 2>&1 > /dev/null
-export SPARK_HOME=$(pwd)/spark-1.6.0-bin-hadoop2.6
-export PATH=${SPARK_HOME}/bin:${PATH}
