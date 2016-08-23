159e102c7047ce4019b198c15557adfde96addb6
Mridul Jain <mridul@yahoo-inc.com>
2016-08-22 22:25:46 -0700

    Python Api Test

--- scripts/travis/travis_install.sh
+++ scripts/travis/travis_install.sh
@@ -10,9 +10,5 @@ rm -rf $CONDA_DIR
 # invoke BVLC caffe scripts
 ./caffe-public/scripts/travis/travis_install.sh
 export CAFFE_ON_SPARK=$(pwd)
-${CAFFE_ON_SPARK}/scripts/local-setup-hadoop.sh 2>&1 > /dev/null
-export HADOOP_HOME=$(pwd)/hadoop-2.6.4
-export PATH=${HADOOP_HOME}/bin:${PATH}
 ${CAFFE_ON_SPARK}/scripts/local-setup-spark.sh 2>&1 > /dev/null
-export SPARK_HOME=$(pwd)/spark-1.6.0-bin-hadoop2.6
-export PATH=${SPARK_HOME}/bin:${PATH}
+
