7459683afd140159dd08d17fd6016b7789db95a4
Arun Das <yab277@my.utsa.edu>
2016-10-09 18:15:52 -0500

    Create local-setup-hadoop.sh
    hadoop v2.7.1

---
+++ scripts/scripts_object_storage/openstack_swift/local-setup-hadoop.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Copyright 2016 Yahoo Inc.
+# Licensed under the terms of the Apache 2.0 license.
+# Please see LICENSE file in the project root for terms.
+#
+# This script install Hadoop locally
+
+wget http://apache.mirrors.tds.net/hadoop/common/hadoop-2.7.1/hadoop-2.7.1.tar.gz
+gunzip hadoop-2.7.1.tar.gz 
+tar -xvf hadoop-2.7.1.tar
